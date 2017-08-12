3ed9f50782487b3af97a7279d232ba9813386ee4
Lin Sixin <sparrowxin@sina.cn>
2017-08-12 23:21:14 +0800

    add a new exception thrown when read in a new line but not request first line

---
+++ src/main/scala/exception/IncorrectRequest.scala
@@ -0,0 +1,12 @@
+//package exception
+//
+///**
+//  * Created by sparr on 2017/8/12.
+//  */
+//class IncorrectRequest(begin:String) extends Exception{
+//  override def getMessage: String = {
+//    begin +
+//      "is not a valid begin of a new request \r\n" +
+//      super.getMessage
+//  }
+//}
