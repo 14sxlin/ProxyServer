89b196b9995284545b2c73f61f6a829e301e7fa6
Lin Sixin <sparrowxin@sina.cn>
2017-08-13 22:14:25 +0800

    update the process to parse request from client

--- src/main/scala/connection/SocketConnection.scala
+++ src/main/scala/connection/SocketConnection.scala
@@ -1,13 +1,16 @@
 package connection
 
-import java.io.{BufferedReader, InputStreamReader, PrintWriter}
+import java.io._
 import java.net.Socket
 
-import entity.Response
+import constants.HttpRequestMethod
+import entity.{Request, Response}
 import handler.header.DefaultHeaderChain
 import org.slf4j.LoggerFactory
 import task.TaskFactory
 import utils.http.RequestBuilder
+
+import scala.collection.mutable.ArrayBuffer
 
 
 /**
@@ -20,17 +23,17 @@ class SocketConnection(val socket:Socket) {
   private val logger = LoggerFactory.getLogger(getClass)
 
   private var readThread: Thread = _
-  private var reader : BufferedReader = _
   private var writer : PrintWriter = _
+  private var in : InputStream = _
+  private var dontCloseConnection2Client = false
 
   def startListen():Unit = {
+    logger.info(s"start read in : ${socket.getInetAddress}")
     setupStream()
     startListenThread()
   }
 
   private def setupStream() : Unit = {
-    reader = new BufferedReader(
-      new InputStreamReader(socket.getInputStream))
     writer = new PrintWriter(socket.getOutputStream)
     logger.debug("setup stream success")
   }
@@ -45,22 +48,59 @@ class SocketConnection(val socket:Socket) {
     readThread.start()
   }
 
-  private def listenAndBuildRequestTask() = {
-    val request = RequestBuilder.buildRequest(reader,
+  private def listenAndBuildRequestTask() : Unit = {
+
+    var request = RequestBuilder.buildRequest(readRequestRawString(),
       DefaultHeaderChain.firstOfRequestHeaderHandlerChain)
+
+    if(request.method == HttpRequestMethod.CONNECT){
+//      responseProxyConnect(request)
+//      request = RequestBuilder.buildRequest(reader,
+//        DefaultHeaderChain.firstOfRequestHeaderHandlerChain)
+      logger.debug("skip connect")
+      return
+    }
     val task = TaskFactory.createTask(request)
     task.onSuccess = response2Client
+    task.onFail = (e) =>{
+      logger.error("",e)
+      closeConnection()
+    }
     task.begin()
+
+  }
+
+  private def readRequestRawString() = {
+    val requestRawData = new Array[Byte](1024*100)
+    in = socket.getInputStream
+    in.read(requestRawData,0,requestRawData.length)
+    val data = new String(requestRawData).trim
+    logger.info(s"raw request: $data")
+    data
+  }
+
+  private def responseProxyConnect(request:Request) : Boolean = {
+    val success200 = "HTTP/1.1 200 Connection established\r\n"
+    writer.append(success200)
+    writer.flush()
+    dontCloseConnection2Client = true
+    true
   }
 
   private def response2Client(response: Response) = {
+    logger.info(s"response length : ${response.body.length}")
     val handler = DefaultHeaderChain.firstOfResponseHeaderHandlerChain
     val newHeader = handler.handle(response.headers)
     response.headers = newHeader
     writer.append(response.mkString)
     writer.flush()
+
+    closeConnection()
+  }
+
+  private def closeConnection(): Unit ={
     writer.close()
-    reader.close()
+    in.close()
     socket.close()
   }
 
