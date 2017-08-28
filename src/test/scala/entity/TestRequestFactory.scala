6e7ae48053028f2c2d56eddd588251c8d483dfee
Lin Sixin <sparrowxin@sina.cn>
2017-08-28 09:41:27 +0800

    add TestRequestFactory

--- src/test/scala/entity/TestRequestFactory.scala
+++ src/test/scala/entity/TestRequestFactory.scala
@@ -9,7 +9,7 @@ import org.scalatest.FunSuite
 class TestRequestFactory extends FunSuite {
 
 
-  test("buildRequest") {
+  test("buildRequest about body") {
 
     val emptyBodyRequest = Array(
       "GET http://localhost:9000/ HTTP/1.1\nHost: localhost:9000\n",
@@ -38,5 +38,10 @@ class TestRequestFactory extends FunSuite {
   test("trim \r\n"){
     val data = "\r\n\r\n"
     assert(data.trim == "")
+  }
+
+  test("test bug : buildRequest"){
+    val raw = "POST http://qurl.f.360.cn/wdinfo.php HTTP/1.1\nHost: qurl.f.360.cn\nAccept: */*\nProxy-Connection: Keep-Alive\nCache-Control: no-cache\nContent-Type: application/octet-stream\nContent-Length: 246\n\n\u001E\n�o�U��q�e\"\u0006�d~*�u�;�P\u007Fj7\u0014�g�T���GZ�L��\u05FFm\u0005�ha�\u0002�ʺES�05\"�\uEBE5��\u001C_�<`�\u0006�6\u0014҅�r��a�E����u�!�)�c[���_aHY�:�kZ��~B�@��Û\u000616bs����;�[�oD��T�#���x�f&Uz���C�b�\u0014`�"
+    RequestFactory.buildRequest(raw)
   }
 }
