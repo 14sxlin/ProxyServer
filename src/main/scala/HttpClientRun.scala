5b7bd6e0f442f618245edd406e980ccbcc5a45e5
Lin Sixin <sparrowxin@sina.cn>
2017-09-01 13:17:29 +0800

    try to simulate the situation that request consumer doesn't work

--- src/main/scala/HttpClientRun.scala
+++ src/main/scala/HttpClientRun.scala
@@ -1,6 +1,9 @@
 import java.net.SocketException
 
+import http.{ConnectionPoolingClient, RequestProxy}
 import mock.client.HttpClientMock
+import org.apache.http.client.methods.HttpGet
+import org.apache.http.client.protocol.HttpClientContext
 import org.slf4j.LoggerFactory
 
 /**
@@ -13,11 +16,9 @@ object HttpClientRun extends App{
   val getPort = 9000
   val proxyPort = 689
   val proxy = "localhost"
-  val targetURI = "/"
-  val targetPort = 8080
-  val targetHost = "localhost"
   val httpClient = new HttpClientMock
-  val postUri = "/LoginDemo/login.do"
+  val postUrl = "http://localhost:8080/LoginDemo/LoginDemo/login.do"
+  val getUrl = "http://localhost:8080/LoginDemo/load?time=4"
 
   def get(url:String) : Thread = {
     val doGet = new Runnable {
@@ -30,23 +31,18 @@ object HttpClientRun extends App{
     doGetThread
   }
 
-  def doGetByProxy(targetURI:String,onSuccess: (String)=>Unit):Thread = {
-    @volatile var success = false
+  val httpGet = new HttpGet(getUrl)
+  val newHttpPooling = new ConnectionPoolingClient
+  val requestProxy = new RequestProxy(newHttpPooling)
+  val httpContext = HttpClientContext.create()
+  def doGetByProxyThread(targetURI:String, onSuccess: (String)=>Unit):Thread = {
     val task = new Runnable {
-      override def run() : Unit = {
-        while(!success){
-          try{
-            val result = httpClient.doGetByProxyWithHttp(targetHost, 8080, proxy, proxyPort, targetURI)
-            logger.info(s"receive data length = ${result.length}")
-            onSuccess(result)
-            Thread.sleep(5000)
-//            success = true
-          }catch {
-            case e:SocketException =>
-              logger.error(s"socket error, ${e.getMessage}")
-              logger.info("retry 10 seconds later")
-              Thread.sleep(10000)
-          }
+      override def run() : Unit = runAndRetryWhenFail{
+        runForTimes(1){
+          val result = httpClient.doGetByProxyWithHttp( proxy, proxyPort, targetURI)
+          //val proxyRequest = httpClient.buildProxyRequest(proxy, proxyPort,httpGet)
+          //httpClient.doRequestProxyInPool(proxyRequest,requestProxy,httpContext)
+          logger.info(s"receive data length = ${result.length}")
         }
       }
     }
@@ -54,37 +50,49 @@ object HttpClientRun extends App{
   }
 
   val params = Array("username"-> "admin","password"->"123")
-  def doPostByProxy() : Thread = {
+  def doPostByProxyThread() : Thread = {
     val task = new Runnable {
-      override def run() : Unit = {
-        while(true){
-          try{
-            val result = httpClient.doPostByProxyWithHttp(
-              targetHost,targetPort,proxy,proxyPort,postUri,params)
-            logger.info(s"receive data length = ${result.length}")
-            Thread.sleep(5000)
-          }catch {
-            case e:SocketException =>
-              logger.error(s"socket error ${e.getMessage}")
-              logger.info("retry 10 seconds later")
-              Thread.sleep(10000)
-          }
-
+      override def run() : Unit =
+        runAndRetryWhenFail {
+          val result = httpClient.doPostByProxyWithHttp(
+            proxy,proxyPort,postUrl,params)
+          logger.info(s"receive data length = ${result.length}")
+          Thread.sleep(5000)
         }
-      }
     }
     new Thread(task)
   }
 
-//  val t1 = doGetByProxy("/");t1.start()
-
-  val onSuccess = (result:String) =>{
-//    println(result)
+  def runAndRetryWhenFail(run : => Unit): Unit ={
+    try{
+      run
+    }catch {
+      case e:SocketException =>
+        logger.error(s"socket error, ${e.getMessage}")
+        logger.info("retry 10 seconds later")
+        Thread.sleep(10000)
+    }
   }
 
-  val t2 = doGetByProxy("/LoginDemo/pic/1.jpg",onSuccess);t2.start()
-//  t1.join()
-  t2.join()
+  def runForTimes(time:Int)(run: => Unit): Unit ={
+    for( _ <- 0 to time){
+      run
+    }
+  }
+//  val t1 = doGetByProxyThread("/");t1.start()
+
+  val onSuccess = (result:String) =>{
+    val min = Math.min(result.length,200)
+    println(result.substring(0,min))
+  }
+
+  runForTimes(20){
+    val t2 = doGetByProxyThread(getUrl,onSuccess);t2.start()
+    //  t1.join()
+
+  }
+
+  Thread.sleep(6000)
 
 }
 
