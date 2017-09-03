da6c5e07cc1fcdd9fcf6e7f8f3535a7f037dfd1c
Lin Sixin <sparrowxin@sina.cn>
2017-09-03 16:26:25 +0800

    update TestRequestQueue

--- src/test/scala/connection/TestRequestQueue.scala
+++ src/test/scala/connection/TestRequestQueue.scala
@@ -1,5 +1,7 @@
 package connection
 
+import java.util.concurrent.ArrayBlockingQueue
+
 import entity.request.Request
 import connection.dispatch.RequestQueue
 import org.apache.http.client.methods.HttpGet
@@ -11,16 +13,31 @@ import org.scalatest.FunSuite
 class TestRequestQueue extends FunSuite{
 
   test("test take if empty queue"){
-    val testQueue = new RequestQueue
-    val request = new HttpGet("")
-    testQueue.put(request)
-    while(true){
-      testQueue.take()
-      print("take success ")
-      // so strange that it won't output until you stop
-      // the progress
+    val queue = new ArrayBlockingQueue[String](20)
+    val consumerRun = new Runnable {
+      override def run(): Unit = {
+        while(true){
+          val str = queue.take()
+          print(s"take $str")
+          // so strange that it won't output until you stop
+          // the progress
+        }
+      }
+    }
+    val providerRun = new Runnable {
+      override def run(): Unit = {
+        while(true){
+          queue.put(""+Math.random())
+        }
+      }
     }
 
+    val consumerThread1 = new Thread(consumerRun);consumerThread1.start()
+    val consumerThread2 = new Thread(consumerRun);consumerThread2.start()
+    val proThread = new Thread(providerRun);proThread.start()
+
+    consumerThread1.join()
+    proThread.join()
   }
 
 }
